e9629525a166033e6dcbf423bd34ff422936dfcd
Krishna Meena <106601578+Z3rOGoku@users.noreply.github.com>
2023-09-07 18:33:48 +0530

    done

---
+++ script.js
@@ -0,0 +1,229 @@
+// its make a favourites meal array if its not exist in local storage
+if (localStorage.getItem("favouritesList") == null) {
+    localStorage.setItem("favouritesList", JSON.stringify([]));
+}
+
+// its fetch meals from api and return it
+async function fetchMealsFromApi(url,value) {
+    const response=await fetch(`${url+value}`);
+    const meals=await response.json();
+    return meals;
+}
+
+
+
+// its show's all meals card in main acording to search input value
+function showMealList(){
+    let inputValue = document.getElementById("my-search").value;
+    let arr=JSON.parse(localStorage.getItem("favouritesList"));
+    let url="https://www.themealdb.com/api/json/v1/1/search.php?s=";
+    let html = "";
+    let meals=fetchMealsFromApi(url,inputValue);
+    meals.then(data=>{
+        if (data.meals) {
+            data.meals.forEach((element) => {
+                let isFav=false;
+                for (let index = 0; index < arr.length; index++) {
+                    if(arr[index]==element.idMeal){
+                        isFav=true;
+                    }
+                }
+                if (isFav) {
+                    html += `
+                <div id="card" class="card mb-3" style="width: 20rem;">
+                    <img src="${element.strMealThumb}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${element.strMeal}</h5>
+                        <div class="d-flex justify-content-between mt-5">
+                            <button type="button" class="btn btn-outline-light" onclick="showMealDetails(${element.idMeal})">More Details</button>
+                            <button id="main${element.idMeal}" class="btn btn-outline-light active" onclick="addRemoveToFavList(${element.idMeal})" style="border-radius:50%"><i class="fa-solid fa-heart"></i></button>
+                        </div>
+                    </div>
+                </div>
+                `;
+                } else {
+                    html += `
+                <div id="card" class="card mb-3" style="width: 20rem;">
+                    <img src="${element.strMealThumb}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${element.strMeal}</h5>
+                        <div class="d-flex justify-content-between mt-5">
+                            <button type="button" class="btn btn-outline-light" onclick="showMealDetails(${element.idMeal})">More Details</button>
+                            <button id="main${element.idMeal}" class="btn btn-outline-light" onclick="addRemoveToFavList(${element.idMeal})" style="border-radius:50%"><i class="fa-solid fa-heart"></i></button>
+                        </div>
+                    </div>
+                </div>
+                `;
+                }  
+            });
+        } else {
+            html += `
+            <div class="page-wrap d-flex flex-row align-items-center">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-md-12 text-center">
+                            <span class="display-1 d-block">404</span>
+                            <div class="mb-4 lead">
+                                The meal you are looking for was not found.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `;
+        }
+        document.getElementById("main").innerHTML = html;
+    });
+}
+
+
+
+//its shows full meal details in main
+async function showMealDetails(id) {
+    let url="https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
+    let html="";
+    await fetchMealsFromApi(url,id).then(data=>{
+        html += `
+          <div id="meal-details" class="mb-5">
+            <div id="meal-header" class="d-flex justify-content-around flex-wrap">
+              <div id="meal-thumbail">
+                <img class="mb-2" src="${data.meals[0].strMealThumb}" alt="" srcset="">
+              </div>
+              <div id="details">
+                <h3>${data.meals[0].strMeal}</h3>
+                <h6>Category : ${data.meals[0].strCategory}</h6>
+                <h6>Area : ${data.meals[0].strArea}</h6>
+              </div>
+            </div>
+            <div id="meal-instruction" class="mt-3">
+              <h5 class="text-center">Instruction :</h5>
+              <p>${data.meals[0].strInstructions}</p>
+            </div>
+            <div class="text-center">
+              <a href="${data.meals[0].strYoutube}" target="_blank" class="btn btn-outline-light mt-3">Watch Video</a>
+            </div>
+          </div>
+        `;
+    });
+    document.getElementById("main").innerHTML=html;
+}
+
+
+
+
+// its shows all favourites meals in favourites body
+async function showFavMealList() {
+    let arr=JSON.parse(localStorage.getItem("favouritesList"));
+    let url="https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
+    let html="";
+    if (arr.length==0) {
+        html += `
+            <div class="page-wrap d-flex flex-row align-items-center">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-md-12 text-center">
+                            <span class="display-1 d-block">404</span>
+                            <div class="mb-4 lead">
+                                No meal added in your favourites list.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `;
+    } else {
+        for (let index = 0; index < arr.length; index++) {
+            await fetchMealsFromApi(url,arr[index]).then(data=>{
+                html += `
+                <div id="card" class="card mb-3" style="width: 20rem;">
+                    <img src="${data.meals[0].strMealThumb}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${data.meals[0].strMeal}</h5>
+                        <div class="d-flex justify-content-between mt-5">
+                            <button type="button" class="btn btn-outline-light" onclick="showMealDetails(${data.meals[0].idMeal})">More Details</button>
+                            <button id="main${data.meals[0].idMeal}" class="btn btn-outline-light active" onclick="addRemoveToFavList(${data.meals[0].idMeal})" style="border-radius:50%"><i class="fa-solid fa-trash"></i></button> 
+                        </div> 
+                    </div>
+                </div>
+                `;
+            });   
+        }
+    }
+    document.getElementById("favourites-body").innerHTML=html;
+}
+
+
+
+
+
+
+//its adds and remove meals to favourites list
+function addRemoveToFavList(id) {
+    let arr=JSON.parse(localStorage.getItem("favouritesList"));
+    let contain=false;
+    for (let index = 0; index < arr.length; index++) {
+        if (id==arr[index]) {
+            contain=true;
+        }
+    }
+    if (contain) {
+        let number = arr.indexOf(id);
+        arr.splice(number, 1);
+        alert("your meal removed from your favourites list");
+    } else {
+        arr.push(id);
+        alert("your meal add your favourites list");
+    }
+    localStorage.setItem("favouritesList",JSON.stringify(arr));
+    showMealList();
+    showFavMealList();
+}
+
+// Dark/light Mode button
+var colorToggleButton = document.getElementById("color-toggle");
+
+// Set an initial background color (black)
+var isWhite = false;
+updateBackgroundColor();
+
+// Add a click event listener to the button
+colorToggleButton.addEventListener("click", function () {
+    isWhite = !isWhite; // Toggle the flag
+    updateBackgroundColor();
+});
+
+// Function to update the background color based on the flag
+function updateBackgroundColor() {
+    var navbar = document.getElementById("nav");
+    var heading = document.getElementById("heading");
+    
+    if (isWhite) {
+        document.body.style.backgroundColor = "#618db8";
+        navbar.style.backgroundColor = "white"; // Change navbar background color to white
+        navbar.style.color = "black"; // Change navbar text color to black
+        heading.style.color = "black"; // Change heading text color to black
+
+        colorToggleButton.innerText = "DARK MODE 🌚";
+    } else {
+        document.body.style.backgroundColor = "rgb(36, 36, 36)";
+        navbar.style.backgroundColor = "#007BFF"; // Change navbar background color back to default (blue in this example)
+        navbar.style.color = "#fff"; // Change navbar text color back to default
+        heading.style.color = "#fff"; // Change heading text color back to default
+        colorToggleButton.innerText = "LIGHT MODE ☀️";
+    }
+}
+
+//change emoji after a time
+function changeEmoji() {
+    const emojiElement = document.getElementById("emoji");
+    const emojis = ["😋"," 🍴", "🍔"]; // Define the emojis you want to cycle through
+    let currentIndex = 0;
+
+    setInterval(function () {
+        emojiElement.textContent = emojis[currentIndex];
+        currentIndex = (currentIndex + 1) % emojis.length;
+    }, 400); // Change the emoji every 0.4 second (adjust the interval as needed)
+}
+
+// Calling the function to start the emoji change animation
+changeEmoji();
